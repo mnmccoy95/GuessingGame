6443390b2c57ec0fa3aa9ede4babca9cd3eae9a9
mnmccoy95 <mnmccoy95@gmail.com>
2020-11-16 11:02:40 -0600

    difficulties implemented

--- Program.cs
+++ Program.cs
@@ -7,19 +7,56 @@ namespace Guess
         static void Main(string[] args)
         {
             int guessAmount = 0;
-            Play(guessAmount);
+            int allowedGuesses = 0;
+            Difficulty(allowedGuesses, guessAmount);
         }
-
-        static void Play(int guesses)
+        static void Difficulty(int allowedGuesses, int guesses)
         {
-            Console.WriteLine("~Welcome to Secret Number Game~");
+            Console.WriteLine(@"~Welcome to Secret Number Game~
+            Choose Your Difficulty:
+            1. Easy
+            2. Medium
+            3. Hard");
+            Console.Write("Difficulty: ");
+            string difficultyChosen = Console.ReadLine();
+            int difficultyInt = 0;
+            bool isDiffNumber = int.TryParse(difficultyChosen, out difficultyInt);
+
+            while(!isDiffNumber | difficultyInt > 3)
+            {
+                Console.Write("Difficulty: ");
+                difficultyChosen = Console.ReadLine();
+                isDiffNumber = int.TryParse(difficultyChosen, out difficultyInt);
+            }
+
+            if(isDiffNumber == true)
+            {
+                if(difficultyInt == 1)
+                {
+                    allowedGuesses = 8;
+                    Play(guesses, allowedGuesses);
+                }
+                if(difficultyInt == 2)
+                {
+                    allowedGuesses = 6;
+                    Play(guesses, allowedGuesses);
+                }
+                if(difficultyInt == 3)
+                {
+                    allowedGuesses = 4;
+                    Play(guesses, allowedGuesses);
+                }
+            }
+
+        }
+        static void Play(int guesses, int allowedGuesses)
+        {
             int secretNumber = Number();
-            Console.WriteLine(secretNumber);
             int answerInt = 0;
 
-            while(guesses < 3 && secretNumber != answerInt)
+            while(guesses < allowedGuesses && secretNumber != answerInt)
             {
-            Console.Write($"Guess a Number! Chances Left({3 - guesses}) : ");
+            Console.Write($"Guess a Number! Chances Left({allowedGuesses - guesses}) : ");
             string answer = Console.ReadLine();
             bool isNumber = int.TryParse(answer, out answerInt);
 
